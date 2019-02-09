99aae9463b6fe04f047f57611241735e356979be
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-02-09 02:15:22 -0800

    fix paths

--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ app.use(bodyParser.json());
 
 routes(app);
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 app.listen(PORT, () => {

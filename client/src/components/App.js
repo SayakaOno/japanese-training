51deac65819a1f9eebfa2c83699b59c1919c7225
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-02-09 13:57:50 -0800

    avoid route 'quiz' conflict between server and client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { Router, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import ShowQuiz from "./ShowQuiz";
-import Setting from "./Setting";
-import Result from "./Result";
-import history from "../history";
-import ShowQuizList from "./ShowQuizList";
-import { countTime } from "../actions";
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import ShowQuiz from './ShowQuiz';
+import Setting from './Setting';
+import Result from './Result';
+import history from '../history';
+import ShowQuizList from './ShowQuizList';
+import { countTime } from '../actions';
 
 class App extends React.Component {
   constructor(props) {
@@ -28,10 +28,10 @@ class App extends React.Component {
     return (
       <Router history={history}>
         <React.Fragment>
-          <Route path="/" exact component={ShowQuizList} />
-          <Route path="/setting" exact component={Setting} />
-          <Route path="/quiz" component={ShowQuiz} />
-          <Route path="/result" component={Result} />
+          <Route path='/' exact component={ShowQuizList} />
+          <Route path='/setting' exact component={Setting} />
+          <Route path='/practice' component={ShowQuiz} />
+          <Route path='/result' component={Result} />
         </React.Fragment>
       </Router>
     );

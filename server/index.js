88047cc07c8c94abdbe141637ac8e5ff34772161
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-02-08 21:16:12 -0800

    ready for deploy

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import routes from './src/routes';
 import { mongoDBAddress } from './config';
 
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 // mongo connection
@@ -19,6 +20,12 @@ routes(app);
 
 app.get('/', (req, res) => {
   res.send(`Node and express server is running on port ${PORT}`);
+});
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {

40e0da9053645ad7ea6e6d4a2b367b63ed0180a5
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-02-08 22:54:06 -0800

    change the way to import 'path'

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
+import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import routes from './src/routes';
 import { mongoDBAddress } from './config';
 
 const app = express();
-const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 // mongo connection

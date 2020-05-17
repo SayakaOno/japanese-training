5f97094e216718b3f2872454a72a04989464881e
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-05-16 23:49:27 -0700

    create a first test using puppeteer

---
+++ client/src/components/App.test.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+describe('H1 Text', () => {
+  test('h1 loads correctly', async () => {
+    let browser = await puppeteer.launch({
+      headless: false
+    });
+    let page = await browser.newPage();
+
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('h1');
+
+    const html = await page.$eval('h1', e => e.innerHTML);
+    expect(html).toBe('Japanese speaking training');
+
+    browser.close();
+  }, 16000);
+});
